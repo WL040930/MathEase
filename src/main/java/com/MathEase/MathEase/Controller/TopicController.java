4d602a22885b39e43479c78a9e6e88f80d8ca21b
WL040930 <152003703+WL040930@users.noreply.github.com>
2024-05-06 23:47:52 +0800

    Doing Link Viewing

--- src/main/java/com/MathEase/MathEase/Controller/TopicController.java
+++ src/main/java/com/MathEase/MathEase/Controller/TopicController.java
@@ -1,17 +1,29 @@
 package com.MathEase.MathEase.Controller;
 
+import com.MathEase.MathEase.Model.Link;
+import com.MathEase.MathEase.Model.Topic;
+import com.MathEase.MathEase.Service.LinkService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import com.MathEase.MathEase.Repository.TopicRepository;
+
+import java.util.List;
 
 @Controller
 public class TopicController {
 
     @Autowired
     private MenuController menuController;
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Autowired
+    private LinkService linkService;
 
     @GetMapping("topic/{topicId}")
     public String topic(@PathVariable Long topicId,
@@ -25,6 +37,13 @@ public class TopicController {
         if (session.getAttribute("role").equals("student")) {
             menuController.setMenuBar(session, model);
             setTopicAttributes(topicId, model);
+
+            Topic topic = topicRepository.findById(topicId).get();
+
+            List<Link> links = linkService.getLinksByTopic(topic);
+
+            model.addAttribute("topics", topic);
+
             return "student/student-topic";
         } else {
             menuController.setMenuBar(session, model);
