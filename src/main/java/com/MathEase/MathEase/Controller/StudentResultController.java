5c43414552af353bc1fa253edfbc9b97c71d951e
WL040930 <152003703+WL040930@users.noreply.github.com>
2024-05-07 21:39:27 +0800

    Updated Student Results

--- src/main/java/com/MathEase/MathEase/Controller/StudentResultController.java
+++ src/main/java/com/MathEase/MathEase/Controller/StudentResultController.java
@@ -1,6 +1,7 @@
 package com.MathEase.MathEase.Controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StudentResultController {
 
+    @Autowired
+    private MenuController menuController;
+
     @GetMapping("/student/quiz/result")
     public String getQuizResult(HttpSession session, Model model) {
 
         if (session.getAttribute("userId") == null || !session.getAttribute("role").equals("student")) {
             return "redirect:/login";
         }
+
+        menuController.setMenuBar(session,model);
 
         return "student/student-quiz-results";
     }
