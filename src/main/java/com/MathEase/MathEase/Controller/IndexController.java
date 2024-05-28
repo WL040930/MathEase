16aa300546481d1af382033e782adf6df476a824
WL040930 <152003703+WL040930@users.noreply.github.com>
2024-05-28 21:18:42 +0800

    Completed System

--- src/main/java/com/MathEase/MathEase/Controller/IndexController.java
+++ src/main/java/com/MathEase/MathEase/Controller/IndexController.java
@@ -1,13 +1,25 @@
 package com.MathEase.MathEase.Controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String showHomePage() {
+    public String showHomePage(HttpSession session, Model model) {
         return "index/index";
+    }
+
+    @GetMapping("/problem")
+    public String showProblemPage() {
+        return "index/problem";
+    }
+
+    @GetMapping("/solution")
+    public String showSolutionPage() {
+        return "index/solution";
     }
 }
