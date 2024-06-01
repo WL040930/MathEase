38548cba900dc1b87acc6c904e30f513224686f0
WL040930 <152003703+WL040930@users.noreply.github.com>
2024-06-01 12:09:27 +0800

    Completed AI ChatModel

--- src/main/java/com/MathEase/MathEase/Controller/UserController.java
+++ src/main/java/com/MathEase/MathEase/Controller/UserController.java
@@ -1,12 +1,9 @@
 package com.MathEase.MathEase.Controller;
 
+import com.MathEase.MathEase.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
-import com.MathEase.MathEase.Model.User;
-import com.MathEase.MathEase.Service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
